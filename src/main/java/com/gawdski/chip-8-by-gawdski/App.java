0aa6b391e6348858c9127b00174d5a7f392ef246
agawda <a.m.gawda@gmail.com>
2017-02-02 18:34:59 +0100

    initial commit

---
+++ src/main/java/com/gawdski/chip-8-by-gawdski/App.java
@@ -0,0 +1,13 @@
+package com.gawdski.chip-8-by-gawdski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
